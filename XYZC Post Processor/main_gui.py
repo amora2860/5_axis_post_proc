fa2d9ae4d25c1b9eaa42d9e315552caccc6adf2e
AJM <andrewmora2860@gmail.com>
2018-12-23 09:09:56 -0500

    I have seperated out the evaluation function but it will need to be even more divided up. I need to add a messege box in properly so that the program tells when the process is complete. Also I need to add several different types of angle evaluation the C axis will have to do.

--- XYZC Post Processor/main_gui.py
+++ XYZC Post Processor/main_gui.py
@@ -2,8 +2,30 @@ from tkinter import *
 from tkinter.filedialog import askopenfilename
 from tkinter.messagebox import showerror
 import math
+from tkinter import messagebox #used for the message box
+
+# this is a list of all the global variables used.
+global listG0
+global listG2
+global x_1
+global y_1
+global x_2
+global y_2
+global i_2
+global j_2
+global t
+global origin_x
+global origin_y
+
+
+
 
 #There must be functions added to this file so that there is some organization
+#Several things to need be evaluated
+# 1 If there needs to be an initial start rotation of the C axis
+# 2 to to line rotation of the C axis
+
+
 
 class MyFrame(Frame):
 
@@ -57,151 +79,144 @@ class MyFrame(Frame):
         #  this creates a new file and ensures its name is the name of the selected one with _postproc.ngc at the end.
         f = open('{0}.ngc'.format(file_name + "_postproc"), "w")
 
+        #function for evalauting G-code lines and calculations
+        g_eval(f,data)
+
+        # A message box will appear to indicate that the G-code processing has been completed
+
+        #this is being done wrong it needs to be apart of the main gui but still be brought up
+        top = Tk()
+        top.geometry("200x200")
+
+        def hello():
+            messagebox.showinfo("XYZC Post Processor", "Process Complete")
+
+        B1 = Button(top, text="Ok", command= quit(hello()))
+        B1.place(x=35, y=50)
+
+        top.mainloop()
 
 
 
-#I need to have this section broken down into different functions and have several sets of logic gates.
-
-
-        #this is a list of all the global variables used.
-        global listG0
-        global listG2
-        global x_1
-        global y_1
-        global x_2
-        global y_2
-        global i_2
-        global j_2
-        global t
-        global origin_x
-        global origin_y
-
-        #this is a list of initialized values of the global variables.
-        x_1 = 0.0
-        y_1 = 0.0
-        x_2 = 0.0
-        y_2 = 0.0
-        i_2 = 0.0
-        j_2 = 0.0
-        listG2 = []
-        listG0 = [0,0,0]
-        t = 0
-
-        #for loop ensures that each line is read in the g-code file
-        for i in data:
-
-            #list2 will be evaluated
-            list2 = i
-
-            #here is where the for loop should go into a class that will process and evaluate the
-            if list2[0:2] == "G0":
-                f.write(str(i))
-                if list2[3:4] == "X":
-                    print("This is the beginning start point.")
-                    t = 1
-                    listG0 = [str(list2[0:2]), str(list2[6:11]), str(list2[15:21])]
-                    print("X1 " + str(listG0[1]) + " Y1 " + str(listG0[2]))
-                    x_1 = float(listG0[1])
-                    y_1 = float(listG0[2])
-
-
-
-            # this section of code looks for G2 and should look for G3 which are arc commands and identifies each
-            # section of the G-code and pulls out the X ,Y, I, J and turns them into float.
-            # At the start of the G-code file is a G20 command which needs to be vetted out.
-            if list2[0:2] == "G2":
-
-                if list2[0:3] != "G20" and list2[0:3] != "G21":
-
-                    listG2 = [str(list2[0:2]), str(list2[6:11]), str(list2[15:21]), str(list2[24:30]), str(list2[34:341])]
-
-                    x_2 = float(listG2[1])
-                    y_2 = float(listG2[2])
-                    i_2 = float(listG2[3])
-                    j_2 = float(listG2[4])
-                    origin_x = x_1 + i_2
-                    origin_y = y_1 + j_2
-
-                    #this section of printing is purely for testing purposes and should be removed with final release
-                    #print("The start point of the arc is (X " + str(x_1) + " Y " + str(y_1) + ")")
-                    #print("The end point is (X " + str(x_2) + " Y " + str(y_2) + ")")
-                    #print("The center of the arc is X " + str(origin_x) + " Y " + str(origin_y))
-                    #print("x_1 " + str(x_1))
-                    #print("y_1 " + str(y_1))
-                    #print("origin x " + str(origin_x))
-                    #print("origin y " + str(origin_y))
-
-                    #This short section of VEC creates the vectors of each line
-                    VEC_1x = origin_x - x_1
-                    VEC_1y = origin_y - y_1
-                    VEC_2x = origin_x - x_2
-                    VEC_2y = origin_y - y_2
-
-                    #Here is the dot product being solved for
-                    dot = (VEC_1x * VEC_2x) + (VEC_1y * VEC_2y)
-
-                    #the magnetude of both vectors is being evaluated
-                    mag_1 = math.sqrt((VEC_1x**2) + (VEC_2x**2))
-                    mag_2 = math.sqrt((VEC_2x**2) + (VEC_2y**2))
-
-                    #finally the magnitudes are multiplyed
-                    mag_mult = mag_1 * mag_2
-
-                    #this equation gives the angle in radians between our two lines
-                    radians = math.acos(dot/mag_mult)
-
-                    #I am converting the radians into degrees
-                    theta = math.degrees(radians)
-
-                    ##this section of printing is purely for testing purposes and should be removed with final release
-                    #print("VEC_1x " + str(VEC_1x))
-                    #print("VEC_1y " + str(VEC_1y))
-                    #print("VEC_2x " + str(VEC_2x))
-                    #print("VEC_2y " + str(VEC_2y))
-                    #print("dot " + str(dot))
-                    #print("mag_1 " + str(mag_1) + " mag_2 " + str(mag_2) + " mag_multi " + str(mag_mult))
-                    #print("The radians are " + str(radians))
-                    #print("The angle is " + str(theta))
-
-
-                    #last step in this process is to add the line "C XXX.XXX" which will be the degrees of rotation of the C axis
-                    f.write( str(list2[:-1]) + "  C " + str("%.3f" % round(theta,3)) +  "\r\n")
-
-                #this section of code takes the second point and pushes to be the new starting point.
-                #the t == 1 flag insures that this part is only entered into if a second point is defined.
-                else:
-                    f.write(str(i))
-                if t == 1:
-                    x_1 = float(listG2[1])
-                    y_1 = float(listG2[2])
-                    print(" This is the new starting point. " + "(X " + str(x_1) + " Y " +str(y_1)+ ")")
-
-
-            #here is where the line for writing each line of code to a new folder and adding the C rotation
-            else:
-                print(i)
-                if list2[0:2] != "G0":
-                    f.write(str(i))
-
-
-
-
-
-
-
-
-
-
-        # how to have this file added to the canvas after it is selected.
+        # if there is an error if the open file an error will be presented
         if fname:
             try:
                 fname_file.close()
-                #f.close()
+                f.close()
             except:  # <- naked except is a bad idea
                 showerror("Open Source File", "Failed to read file\n'%s'" % fname)
             return
 
         return data
+
+
+
+
+#I need to have this section broken down into different functions and have several sets of logic gates.
+
+def g_eval(f,data):
+    # this is a list of initialized values of the global variables.
+    x_1 = 0.0
+    y_1 = 0.0
+    x_2 = 0.0
+    y_2 = 0.0
+    i_2 = 0.0
+    j_2 = 0.0
+    listG2 = []
+    listG0 = [0, 0, 0]
+    t = 0
+
+
+    #for loop ensures that each line is read in the g-code file
+    for i in data:
+
+        #here is where the for loop should go into a class that will process and evaluate the
+        if i[0:2] == "G0":
+            f.write(str(i))
+            if i[3:4] == "X":
+                #set flag
+                t = 1
+                #seperate line into smaller pieces of values
+                listG0 = [str(i[0:2]), str(i[6:11]), str(i[15:21])]
+
+                #saving values into float values for evaluation later on
+                x_1 = float(listG0[1])
+                y_1 = float(listG0[2])
+
+
+
+        # this section of code looks for G2 and should look for G3 which are arc commands and identifies each
+        # section of the G-code and pulls out the X ,Y, I, J and turns them into float.
+        # At the start of the G-code file is a G20 command which needs to be vetted out.
+        if i[0:2] == "G2" or i[0:2] == "G3":
+
+            if i[0:3] != "G20" and i[0:3] != "G21":
+
+                listG2 = [str(i[0:2]), str(i[6:11]), str(i[15:21]), str(i[24:30]), str(i[34:341])]
+
+                x_2 = float(listG2[1])
+                y_2 = float(listG2[2])
+                i_2 = float(listG2[3])
+                j_2 = float(listG2[4])
+                origin_x = x_1 + i_2
+                origin_y = y_1 + j_2
+
+                #This short section of VEC creates the vectors of each line
+                VEC_1x = origin_x - x_1
+                VEC_1y = origin_y - y_1
+                VEC_2x = origin_x - x_2
+                VEC_2y = origin_y - y_2
+
+                #Here is the dot product being solved for
+                dot = (VEC_1x * VEC_2x) + (VEC_1y * VEC_2y)
+
+                #the magnetude of both vectors is being evaluated
+                mag_1 = math.sqrt((VEC_1x**2) + (VEC_2x**2))
+                mag_2 = math.sqrt((VEC_2x**2) + (VEC_2y**2))
+
+                #finally the magnitudes are multiplyed
+                mag_mult = mag_1 * mag_2
+
+                #this equation gives the angle in radians between our two lines
+                radians = math.acos(dot/mag_mult)
+
+                #if the arc command G3 then the theta value needs to be subtracted
+                if i[0:2] == "G3":
+                # I am converting the radians into degrees
+                    theta = -math.degrees(radians)
+                else:
+                #I am converting the radians into degrees
+                    theta = math.degrees(radians)
+
+
+                #last step in this process is to add the line "C XXX.XXX" which will be the degrees of rotation of the C axis
+                f.write( str(i[:-1]) + "  C " + str("%.3f" % round(theta,3)) +  "\r\n")
+
+            #this section of code takes the end point and makes it the new starting point.
+            #the t == 1 flag insures that this part is only entered into if a second point is defined.
+            else:
+                f.write(str(i))
+            if t == 1:
+                x_1 = float(listG2[1])
+                y_1 = float(listG2[2])
+
+
+
+        #here is a catch all of G-codes and line of text that dont meet any other forms of evaluation.
+        else:
+            if i[0:2] != "G0":
+                f.write(str(i))
+
+
+
+
+
+
+
+
+
+
+
 
 
 
